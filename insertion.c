5ed8a7d8d1ac76c559f9bdfd745b2164f7b3aa63
devendra9565 <code.for.dev12@gmail.com>
2025-09-01 15:39:59 +0530

    Program to insert the given elements into an array.

--- insertion.c
+++ insertion.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
 int main() 
 {
-    printf("Hello, World!");
+    int a[50],n,x,i,pos;
+    printf("Enter the number of elements in array: ");
+    scanf("%d", &n);
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++) 
+    {
+        scanf("%d", &a[i]);
+    }
+    printf("Enter the element to insert: ");
+    scanf("%d", &x);
+    printf("Enter the position to insert : ");
+    scanf("%d", &pos);
+        for (i = n; i > pos; i--) 
+        {
+            a[i] = a[i-1];
+        }
+        a[pos] = x;
+        n++;
+        printf("Array after insertion:\n");
+        for (i = 0; i < n; i++)
+        {
+            printf("%d ", a[i]);
+        }
+        printf("\n");
     return 0;
 }
---
+++ vector.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+vector<int>v;
+cout<<"Size of Vector 1:"<<v.size()<<endl;
+cout<<"Capacity of Vector 1:"<<v.capacity()<<endl;
+v.push_back(1);
+cout<<"Size of Vector 1:"<<v.size()<<endl;
+cout<<"Capacity of Vector 1:"<<v.capacity()<<endl;
+v.push_back(3);
+cout<<"Size of Vector 1:"<<v.size()<<endl;
+cout<<"Capacity of Vector 1:"<<v.capacity()<<endl;
+
+return 0;
+}
